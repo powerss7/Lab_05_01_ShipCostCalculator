966d4314443662ccd4a172e0442505b7e1cf7e0a
samuel powers <powerss7@mail.uc.edu>
2022-09-22 13:05:07 -0400

    it worked first try!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double Price = 150;
+        double shippingTax = 0.02;
+
+        if(Price >= 100)
+            System.out.println("Your total price is " + (Price + (Price * 0.02)));
+        else
+            System.out.println("You get free shipping, your total price is " + Price);
     }
 }
